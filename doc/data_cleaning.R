27ff867f4eaf11569e9eb49abb5608c7bedf1a20
Denise Sonia Rahmadina <89030834+denisesonia@users.noreply.github.com>
2023-03-01 21:01:15 -0500

    nlp cleaning

--- doc/data_cleaning.R
+++ doc/data_cleaning.R
@@ -1,6 +1,11 @@
 library(pacman)
 pacman::p_load(dplyr)
 
+library(dplyr)
+library(tm) # for text mining
+library(SnowballC) # for stemming
+library(stringr) # for string manipulation
+
 current_path = rstudioapi::getActiveDocumentContext()$path
 setwd(dirname(current_path ))
 
@@ -20,4 +25,41 @@ merged = merge(x = merged, y = places, by = "place_id")
 
 df = merged[,c("dialogue_id","movie_title","movie_chapter","chapter_id","place_name","place_category","character_name","gender","house","species","dialogue","dialogue_no_spells")]
 
-write.csv(df, "../data/cleaned.csv", row.names=FALSE)
+#write.csv(df, "../data/cleaned.csv", row.names=FALSE)
+
+clean_text <- function(text) {
+  # Convert to lowercase
+  text <- tolower(text)
+  
+  # Remove punctuation
+  text <- str_replace_all(text, "[[:punct:]]", "")
+  
+  # Remove numbers
+  text <- str_replace_all(text, "\\d+", "")
+  
+  # Remove stop words
+  text <- removeWords(text, stopwords("english"))
+  
+  # Stem words
+  text <- wordStem(text)
+  
+  # Remove whitespace
+  text <- str_trim(text)
+  
+  # Return cleaned text
+  return(text)
+}
+
+# Apply cleaning function to text column
+df$dialogue<- iconv(df$dialogue, from = 'ISO-8859-1', to = 'utf8')
+df$dialogue_cleaned <- sapply(df$dialogue, clean_text)
+
+write.csv(df, "../data/cleaned_nlp.csv", row.names=FALSE)
+
+
+
+
+
+
+
+
